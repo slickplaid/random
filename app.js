88c2a6da444a4252114eacafb1b25a6d1f26ba4a
Evan Johnson <slickpla@gmail.com>
2012-03-13 21:43:37 -0500

    files

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var httpProxy = require('http-proxy');
+
+var options = {
+  router: {
+    'ecigtv.com': '127.0.0.1:4000',
+    'lavacorps.internationalvapersclub.com': '127.0.0.1:3001'
+  }
+}
+var proxy = httpProxy.createServer(options);
+proxy.listen(3000);
+
+var app = express.createServer();
+
+app.configure(function() {
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.get('/', function(req, res) {
+
+});
+
+app.get('*', function(req, res) {
+  res.redirect('/');
+});
+
+app.listen(4000);
